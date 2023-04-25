e645ab84a0576ff594e9a37b7831b1019480841c
avsh <avsh@users.noreply.github.com>
2023-04-25 21:59:43 +0300

    enable cs-cart dev mode in image

--- docker/scripts/local_conf.php
+++ docker/scripts/local_conf.php
@@ -1,17 +1,16 @@
 <?php
-if ($_SERVER['REMOTE_ADDR'] == '178.32.220.27') {
+// Turn on the Debug mode for the admin panel and the storefront
+define('DEBUG_MODE', true);
 
-    // Turn on the Debug mode for the admin panel and the storefront
-    define('DEBUG_MODE', true);
+// Use the Development mode to display errors
+define('DEVELOPMENT', true);
 
-    // Use the Development mode to display errors
-    define('DEVELOPMENT', true);
+// Display SMARTY and PHP errors on the screen.
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+ini_set('display_startup_errors', true);
 
-    // Display SMARTY and PHP errors on the screen.
-    error_reporting(E_ALL);
-    ini_set('display_errors', 'on');
-    ini_set('display_startup_errors', true);
+// Disable PHP block caching
+$config['tweaks']['disable_block_cache'] = true;
 
-    // Disable PHP block caching
-    $config['tweaks']['disable_block_cache'] = true;
-}
+$config['resources']['marketplace_url'] .= '?access_token=164a9fe21a79e120a75fdee945e8a6d0';
