3b38f77b1f8b64f4a0c4a8edbbd9909f721c83d7
avsh <avsh@users.noreply.github.com>
2024-02-17 20:11:59 +0300

    use composer to include begateway php api lib

---
+++ app/addons/begateway/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'begateway/cs-cart-payment-module',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'cbb12dae89371960840728590ddd5c6ffb9c076e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../../../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'begateway/begateway-api-php' => array(
+            'pretty_version' => '5.1.0',
+            'version' => '5.1.0.0',
+            'reference' => 'a58aa709182710f8eab692333131995e76beb808',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../begateway/begateway-api-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'begateway/cs-cart-payment-module' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'cbb12dae89371960840728590ddd5c6ffb9c076e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../../../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
